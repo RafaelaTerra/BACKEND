c5dd7a173f3841394d52640422f4cf9da02eab99
Ensley <ensleyfmr@hotmail.com>
2019-01-16 10:32:46 -0200

    Alteração backend funcionando

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,11 +3,11 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server= express()
-const allowCors = require('./cors')
+const allCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
-server.user(allColrs)
+server.use(allCors)
 
 server.listen(port, function(){
     console.log(`BACKEND is running no port ${port}.`)
